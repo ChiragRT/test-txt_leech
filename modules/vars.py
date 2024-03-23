6fa0bd93fa501c17a71acf84d3ca08c047738374
ChiragRT <154085607+ChiragRT@users.noreply.github.com>
2024-03-23 17:32:23 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "25046127"
+api_hash = "b98e578e8b500c77c6a80839dfbdc4a5"
+bot_token = "6807551471:AAHgkkK6BA6UHNx71z7eIpfIc9cVsEODmRQ"
