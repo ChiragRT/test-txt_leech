4c113d3f683c88232e32d02b9efb9ba13514603e
ChiragRT <154085607+ChiragRT@users.noreply.github.com>
2024-03-29 13:04:41 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "25046127"
 api_hash = "b98e578e8b500c77c6a80839dfbdc4a5"
-bot_token = "6893923235:AAGqsnbKLVmkh6r2RaJcUQqIk8Wbrnrg53c"
+bot_token = "7107104145:AAGpdOR5GWT0wxwCttrXvqfwmSZ0BO4mZqE"
